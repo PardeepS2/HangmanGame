780f29cd6a0bc118194953540c26c65bc43c9faa
PardeepS2 <Pardeemaloya@gmail.com>
2017-07-08 18:41:33 -0400

    build a method to generate random word

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -10,9 +10,23 @@ namespace HangmanGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is the demo app");
+            Console.WriteLine("\t\tLet's Play Hangman");
+            Console.WriteLine("\t\t==================");
+            int chances = 5;
+            Generateword();
             Console.ReadKey();
 
         }
+        public static void Generateword() // choose random word from the given array
+        {
+            string word;
+            Random rnd = new Random();
+            string[] listofwords = { "guitar", "saxophone", "trumpet", "xylophone",
+                                      "accordion", "harmonica", "melodica", "sitar" };
+            word = listofwords[rnd.Next(0, listofwords.Length)];
+            Console.WriteLine(word);
+        }
+
+
     }
 }
