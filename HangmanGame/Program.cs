c6eac1dde5c0b2149ade08d91cb74ff16ac63d88
PardeepS2 <Pardeemaloya@gmail.com>
2017-07-09 06:45:21 -0400

    build logic for hangman which contains switch statement.

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -8,25 +8,66 @@ namespace HangmanGame
 {
     class Program
     {
+        static Random rnd = new Random();
+        static int chances = 5;
+        static string selectedword;  // random word choose by comupter
+        static string guessword;
+        
         static void Main(string[] args)
         {
             Console.WriteLine("\t\tLet's Play Hangman");
             Console.WriteLine("\t\t==================");
-            int chances = 5;
-            Generateword();
+           
+            selectedword = Generateword();
+            hangman();
             Console.ReadKey();
 
         }
-        public static void Generateword() // choose random word from the given array
+
+        public static string Generateword() // metod to generate random word from the given array
         {
-            string word;
-            Random rnd = new Random();
+            string word;          
             string[] listofwords = { "guitar", "saxophone", "trumpet", "xylophone",
                                       "accordion", "harmonica", "melodica", "sitar" };
+            
             word = listofwords[rnd.Next(0, listofwords.Length)];
-            Console.WriteLine(word);
+            return word;
         }
 
+        public static void hangman() // build hangman
+        {
+            
+            switch (chances)
+            {
+                case 0:
+                    Console.WriteLine(" O ");
+                    Console.WriteLine("-|-");
+                    Console.WriteLine("/ \\ ");
+                    Console.WriteLine("GAME OVER!");
+                    break;
+                case 1:
+                    Console.WriteLine(" O ");
+                    Console.WriteLine("-|-");
+                    Console.WriteLine("/  ");
+                    break;
+                case 2:
+                    Console.WriteLine(" O ");
+                    Console.WriteLine("-|-");
+                    break;
+                case 3:
+                    Console.WriteLine(" O ");
+                    Console.WriteLine("-|");
+                    break;
+                case 4:
+                    Console.WriteLine(" O ");
+                    Console.WriteLine(" |");
+                    break;
+                case 5:
+                    Console.WriteLine("Looking good!");
+                    break;
+
+            }
+        }
 
     }
 }
